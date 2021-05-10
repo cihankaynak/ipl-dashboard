39ba6181d75c1a63a1243a8e9aa1efa7788309a1
Cihan Kaynak <cihan.kaynak@cerner.com>
2021-05-09 20:45:09 -0500

    Completed Match page.

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import MatchDetailCard from "../components/MatchDetailCard";
 
 const MatchPage = () => {
+  const [matches, setMatches] = useState([]);
+  const { teamName, year } = useParams();
+  useEffect(() => {
+    fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then((matches) => {
+        setMatches(matches);
+      });
+  }, [teamName, year]);
   return (
     <div className="MatchPage">
       <h1>Match Page</h1>
+      {matches.map((match) => (
+        <MatchDetailCard key={match.id} match={match} team={teamName} />
+      ))}
     </div>
   );
 };
